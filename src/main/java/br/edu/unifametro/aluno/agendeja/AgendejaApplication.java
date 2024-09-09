badfb909c49799e8729b6c55690c0a23bc180243
Mat-P1 <mateuspinheiro.peixoto1@gmail.com>
2024-09-09 08:55:32 -0300

    #Project: Initialization and Structure

---
+++ src/main/java/br/edu/unifametro/aluno/agendeja/AgendejaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unifametro.aluno.agendeja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendejaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/unifametro/aluno/agendeja/AgendejaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unifametro.aluno.agendeja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6e2b47e7b7f19651dac9277daeb8a768d1a5fa06
Mat-P1 <mateuspinheiro.peixoto1@gmail.com>
2024-09-10 11:01:26 -0300

    #Configuration: New class (SecurityConfig) created. Basic security configuration for endpoint testing

---
+++ src/main/java/br/edu/unifametro/aluno/agendeja/configuration/SecurityConfig.java
@@ -0,0 +1,30 @@
+package br.edu.unifametro.aluno.agendeja.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.POST, "/users/signup").permitAll()
+        );
+
+        // use HTTP Basic Authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // Disable Cross Site Request Forgery (CSRF)
+        // Generally not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}

35cce6ad7fa05887c25bca0e20408b9ff7f2b10a
Mat-P1 <mateuspinheiro.peixoto1@gmail.com>
2024-09-17 13:41:14 -0300

    #Project: CORS update

--- src/main/java/br/edu/unifametro/aluno/agendeja/controller/UserController.java
+++ src/main/java/br/edu/unifametro/aluno/agendeja/controller/UserController.java
@@ -8,10 +8,7 @@ import br.edu.unifametro.aluno.agendeja.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * UserController handles user-related operations such as sign-up and registration.
@@ -29,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users")
+@CrossOrigin(origins = "http://localhost:5173/")
 @RequiredArgsConstructor
 public class UserController {
 

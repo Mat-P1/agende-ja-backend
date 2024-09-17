73a27896a0fd301929d402714ddda3231526bed2
Mat-P1 <mateuspinheiro.peixoto1@gmail.com>
2024-09-17 15:33:38 -0300

    #Project: CORS update

--- src/main/java/br/edu/unifametro/aluno/agendeja/configuration/CorsConfig.java
+++ src/main/java/br/edu/unifametro/aluno/agendeja/configuration/CorsConfig.java
@@ -11,6 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
     }
 }
